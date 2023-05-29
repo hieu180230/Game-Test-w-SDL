0e70fbb8c79cf09d968e6d09de5d78b901318a08
hieu18022004 <hieu180230@gmail.com>
2023-05-29 18:31:20 +0700

    keyboard key test

--- SDL_Game_Test/main.cpp
+++ SDL_Game_Test/main.cpp
@@ -1,5 +1,8 @@
 #include "SDL.h"
+#include <iostream>
 #include <Windows.h>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
@@ -10,6 +13,8 @@ int main(int argc, char* argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
     bool isRunning = true;
     while (isRunning)
     {
@@ -17,10 +22,24 @@ int main(int argc, char* argv[])
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT) isRunning = false;
+            if (event.type == SDL_MOUSEMOTION)
+            {
+                cout << "Mouse moved\n";
+            }
+            if (event.type == SDL_KEYDOWN)
+            {
+                cout << "Pressed\n";
+                if (event.key.keysym.sym == SDLK_0)
+                {
+                    cout << "0 pressed\n";
+                }
+            }
+            if (event.type == SDL_KEYUP)
+            {
+                cout << "Released\n";
+            }
         }
     }
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
