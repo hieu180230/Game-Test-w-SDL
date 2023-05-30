52e01fba0fe8e4a59716db6cf6b5e4051242e14f
hieu18022004 <hieu180230@gmail.com>
2023-05-30 18:05:20 +0700

    vọc vạch

--- SDL_Game_Test/main.cpp
+++ SDL_Game_Test/main.cpp
@@ -1,4 +1,5 @@
 #include "SDL.h"
+#include "glad/include/glad/glad.h"
 #include <iostream>
 #include <Windows.h>
 
@@ -11,36 +12,53 @@ int main(int argc, char* argv[])
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
 
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Surface* screen; //window surface
+    SDL_Surface* image; //load image
 
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+
+    window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    screen = SDL_GetWindowSurface(window);
+    image = SDL_LoadBMP("resource//cat-owl.bmp");
+    SDL_BlitSurface(image, NULL, screen, NULL);
+    SDL_FreeSurface(image);
+
+    SDL_GLContext context;
+    context = SDL_GL_CreateContext(window);
+
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+    //update window
+    SDL_UpdateWindowSurface(window);
+
+    const Uint8* state = SDL_GetKeyboardState(NULL);
     bool isRunning = true;
     while (isRunning)
     {
+        glViewport(0, 0, WIDTH, HEIGHT);
+
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT) isRunning = false;
-            if (event.type == SDL_MOUSEMOTION)
+            if (event.button.button == SDL_BUTTON_LEFT)
             {
-                cout << "Mouse moved\n";
-            }
-            if (event.type == SDL_KEYDOWN)
-            {
-                cout << "Pressed\n";
-                if (event.key.keysym.sym == SDLK_0)
-                {
-                    cout << "0 pressed\n";
-                }
-            }
-            if (event.type == SDL_KEYUP)
-            {
-                cout << "Released\n";
+                SDL_LockSurface(screen);
+                SDL_memset(screen->pixels, 255, screen->h * screen->pitch);
+                SDL_UnlockSurface(screen);
+                SDL_UpdateWindowSurface(window);
             }
         }
     }
-
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
